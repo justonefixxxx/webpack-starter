70287aed4e59f2fc20544813262692b5a342c989
justonefixxxx <justonefixxxx@gmail.com>
2016-07-02 15:19:07 +0300

    css minifying

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
-gulp.task ('default', function(){
-console.log('default task');
+gulp.task('minify-css', function() {
+    return gulp.src('app/**/*.css')
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(gulp.dest('build/**/'));
 });
+
+gulp.task('default', ['minify-css']);
