780e44621da2526bd6dd8eedc41c55d3524b9c8c
justonefixxxx <justonefixxxx@gmail.com>
2016-07-04 03:22:59 +0300

    js-handling task

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by denis on 7/2/16.
  */
+document.body.onmouseover = (event)=> {
+    let target = event.target;
+    console.log(target);
+    target.style.backgroundColor = 'red';
+    target.classList.toggle('black');
+};
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,41 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', ()=> {
     return gulp.src('app/**/*.css')
-        .pipe(cleanCSS({debug: true}, function(details) {
+        .pipe(concat('bundle.css'))
+        .pipe(cleanCSS({debug: true}, function (details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('build/styles'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/**/*.*', ['minify-css']);
+
+gulp.task('js-handling', ()=> {
+    return gulp.src('app/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('default', ['minify-css', 'watch']);
+gulp.task('serve', ['minify-css', 'js-handling'], ()=> {
+
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch("app/**/*.*", ['minify-css', 'js-handling', browserSync.reload]);
+});
+
+
+gulp.task('default', ['serve']);
